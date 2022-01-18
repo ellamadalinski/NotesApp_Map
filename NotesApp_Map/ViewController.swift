40425295396162f8ce582cd1d9012540d06a3a8a
Ella Madalinski <ella.madalinski24@d155.org>
2022-01-18 08:09:14 -0600

    day 1 maps- learned how to set a span and put down a pin

--- NotesApp_Map/ViewController.swift
+++ NotesApp_Map/ViewController.swift
@@ -6,12 +6,25 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapOutlet: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        mapOutlet.showsUserLocation = true
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = CLLocationCoordinate2D(latitude: 42.2371 , longitude: -88.3226)
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 500, longitudinalMeters: 500)
+        let region2 = MKCoordinateRegion(center: center, span: span)
+        mapOutlet.setRegion(region2, animated: true)
+        
     }
 
 
